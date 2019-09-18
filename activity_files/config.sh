5405737d0744866dac2c431bc3edb62fdacdafc4
steveri <steveri@steveri.com>
2019-09-18 10:17:55 -0700

    new branch

---
+++ activity_files/config.sh
@@ -0,0 +1,12 @@
+activity_file=conv.ai
+module=Tile_MemCore
+
+cat <<EOF
+
+    Hello I am config.sh
+    I can execute the power flow using various activity files e.g.
+
+        tapeout_16/bin/do_power_flow $activity_file $module
+
+EOF
+
