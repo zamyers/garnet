a6554af211da9bdd203cece070b06da8fbd8b49d
norabarlow <kathleen.feng0@gmail.com>
2019-09-18 13:01:06 -0700

    add synthesis back in

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -1,15 +1,8 @@
 source /cad/modules/tcl/init/sh
-#eval `/cad/modules/tcl/modulecmd.tcl bash autoinit`
 module load base
-module load genus
 module load genesis2
 module load incisive/15.20.022
 module load lc
 module load syn/latest
-#module load pts/M-2017.06-SP3
-module load dc_shell/latest
-module load cdesigner/latest
-module load hercules
-module load vcs/latest
-module load starrc/latest
-module load cx/2009.09
+module load genus
+module load innovus/19.10.000
