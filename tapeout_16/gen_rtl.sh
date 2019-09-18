136d913cc058da1c8225ac66f34632381aec0606
norabarlow <kathleen.feng0@gmail.com>
2019-09-18 12:53:31 -0700

    upgrade to python3

--- tapeout_16/gen_rtl.sh
+++ tapeout_16/gen_rtl.sh
@@ -7,7 +7,7 @@ if [ -d "genesis_verif/" ]; then
   rm -rf genesis_verif
 fi
 
-python garnet.py --width 32 --height 16 -v --no_sram_stub
+python3 garnet.py --width 32 --height 16 -v --no_sram_stub
 
 cp garnet.v genesis_verif/garnet.sv
 
