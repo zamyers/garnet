ee555b47669654313c7d64317e55d13fbe37c6ef
norabarlow <kathleen.feng0@gmail.com>
2019-09-18 11:39:18 -0700

    add modules

---
+++ .buildkite/setup.sh
@@ -0,0 +1,17 @@
+source /cad/modules/tcl/init/sh
+#eval `/cad/modules/tcl/modulecmd.tcl bash autoinit`
+module load base
+module load genus
+module load genesis2
+module load incisive/15.20.022
+module load lc
+module load syn/latest
+#module load pts/M-2017.06-SP3
+module load dc_shell/latest
+module load cdesigner/latest
+module load hercules
+module load vcs/latest
+module load starrc/latest
+module load cx/2009.09
+
+export PATH=/nobackupkiwi/kzf/local/bin:$PATH:/cad/synopsys/pts/M-2017.06-SP3/bin
