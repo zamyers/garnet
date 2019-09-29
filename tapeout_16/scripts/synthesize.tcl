f8eebf9755531ec5a9c8487645591dfc263fce42
norabarlow <kathleen.feng0@gmail.com>
2019-09-28 23:41:01 -0700

    clean up activity files, use xrun output with modified instance name

--- tapeout_16/scripts/synthesize.tcl
+++ tapeout_16/scripts/synthesize.tcl
@@ -67,16 +67,16 @@ redirect syn.area1 {report_area -detail}
 write_snapshot -directory results_syn -tag final
 write_design -innovus -basename results_syn/syn_out
 
-set values {harris2}
-foreach v $values {
-    read_saif -instance Tile_MemCore ../../activity_files/${v}.saif
-    report_power -depth 3 -full_instance_names Tile_MemCore > ${v}.power
-}
-
+set values {harris}
 #foreach v $values {
-#    read_saif -update -instance Tile_MemCore ../../activity_files/${v}.saif
+#    read_saif -instance Tile_MemCore ../../activity_files/${v}.saif
+#    report_power -depth 3 -full_instance_names Tile_MemCore > ${v}.power
 #}
-#report_power -depth 3 -full_instance_names Tile_MemCore/MemCore_inst0 > average.power
+
+foreach v $values {
+    read_saif -update -instance Tile_MemCore ../../activity_files/${v}.saif
+}
+report_power -depth 3 -full_instance_names Tile_MemCore/MemCore_inst0 > average.power
 
 #report_power -depth 10 -full_instance_names Tile_MemCore/MemCore_inst0/memory_core_inst0/doublebuffer_control > syn.power1
 #report_power -hier -depth 10 -full_instance_names -detail > syn.power1
