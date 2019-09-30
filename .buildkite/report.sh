e69e9d84eb7183c42f1e95b2e3f3ef4c3ad5889d
norabarlow <kathleen.feng0@gmail.com>
2019-09-30 15:10:57 -0700

    add parse script

--- .buildkite/report.sh
+++ .buildkite/report.sh
@@ -1,9 +1,13 @@
-echo "--- AREA"
+echo "--- AREA SUMMARY"
+python ./.buildkite/parse.py --input=tapeout_16/synth/Tile_MemCore/syn.area
+echo "--- area dump"
 cat tapeout_16/synth/Tile_MemCore/syn.area
 
 FILES=tapeout_16/synth/Tile_MemCore/*.power
 for f in $FILES
 do
-    echo "--- $f"
+    echo "--- $f SUMMARY"
+    python ./.buildkite/parse.py --input=$f
+    echo "--- $f dump"
     cat $f
 done
