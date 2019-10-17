6ff2ce7e3943b007dff500b7f3bac17c69f59a08
Taeyoung Kong <kongty0329@gmail.com>
2019-10-17 16:43:15 -0700

    Add axi fsm

--- global_controller/kratos/axil_if.py
+++ global_controller/kratos/axil_if.py
@@ -1,9 +1,9 @@
 from kratos import *
 
 class Axil:
-    def __init__(self, axil_awidth, axil_dwidth):
-        self.axil_awidth = axil_awidth
-        self.axil_dwidth = axil_dwidth
+    def __init__(self, p_axil_awidth, p_axil_dwidth):
+        self.p_axil_awidth = p_axil_awidth
+        self.p_axil_dwidth = p_axil_dwidth
 
     def input(self):
         class _AxilInput(PortBundle):
@@ -19,18 +19,18 @@ class Axil:
 
     def master(self):
         class _AxilMaster(PortBundle):
-            def __init__(self, axil_awidth, axil_dwidth):
+            def __init__(self, p_axil_awidth, p_axil_dwidth):
                 super().__init__()
 
                 # AW ports
-                self.output("awaddr", axil_awidth)
+                self.output("awaddr", p_axil_awidth)
                 self.output("awprot", 3)
                 self.output("awvalid", 1)
                 self.input("awready", 1)
 
                 # W ports
-                self.output("wdata", axil_dwidth)
-                self.output("wstrb", int(axil_dwidth/8))
+                self.output("wdata", p_axil_dwidth)
+                self.output("wstrb", int(p_axil_dwidth/8))
                 self.output("wvalid", 1)
                 self.input("wready", 1)
 
@@ -40,33 +40,33 @@ class Axil:
                 self.input("bvalid", 1)
 
                 # AR ports
-                self.output("araddr", axil_awidth)
+                self.output("araddr", p_axil_awidth)
                 self.output("arprot", 3)
                 self.output("arvalid", 1)
                 self.input("arready", 1)
 
                 # R ports
-                self.input("rdata", axil_dwidth)
+                self.input("rdata", p_axil_dwidth)
                 self.input("rresp", 2)
                 self.input("rvalid", 1)
                 self.output("rready", 1)
 
-        return _AxilMaster(self.axil_awidth, self.axil_dwidth)
+        return _AxilMaster(self.p_axil_awidth, self.p_axil_dwidth)
 
     def slave(self):
         class _AxilSlave(PortBundle):
-            def __init__(self, axil_awidth, axil_dwidth):
+            def __init__(self, p_axil_awidth, p_axil_dwidth):
                 super().__init__()
 
                 # AW ports
-                self.input("awaddr", axil_awidth)
+                self.input("awaddr", p_axil_awidth)
                 self.input("awprot", 3)
                 self.input("awvalid", 1)
                 self.output("awready", 1)
 
                 # W ports
-                self.input("wdata", axil_dwidth)
-                self.input("wstrb", int(axil_dwidth/8))
+                self.input("wdata", p_axil_dwidth)
+                self.input("wstrb", int(p_axil_dwidth/8))
                 self.input("wvalid", 1)
                 self.output("wready", 1)
 
@@ -76,15 +76,15 @@ class Axil:
                 self.output("bvalid", 1)
 
                 # AR ports
-                self.input("araddr", axil_awidth)
+                self.input("araddr", p_axil_awidth)
                 self.input("arprot", 3)
                 self.input("arvalid", 1)
                 self.output("arready", 1)
 
                 # R ports
-                self.output("rdata", axil_dwidth)
+                self.output("rdata", p_axil_dwidth)
                 self.output("rresp", 2)
                 self.output("rvalid", 1)
                 self.input("rready", 1)
 
-        return _AxilSlave(self.axil_awidth, self.axil_dwidth)
+        return _AxilSlave(self.p_axil_awidth, self.p_axil_dwidth)
--- global_controller/kratos/glc.py
+++ global_controller/kratos/glc.py
@@ -3,21 +3,20 @@ from axil_if import Axil
 from glc_cfg_arbiter import GlcCfgArbiter
 
 class GlobalController(Generator):
-    def __init__(self, axil_awidth, axil_dwidth):
+    def __init__(self, p_axil_awidth, p_axil_dwidth):
         super().__init__("global_controller")
         # Parameters
-        self.axil_awidth = axil_awidth
-        self.axil_dwidth = axil_dwidth
+        self.p_axil_awidth = p_axil_awidth
+        self.p_axil_dwidth = p_axil_dwidth
 
         # Axi-lite interface
-        self.axil_if = Axil(self.axil_awidth, self.axil_dwidth)
+        self.axil_if = Axil(self.p_axil_awidth, self.p_axil_dwidth)
 
         # Port declaration
         self.axil_in = self.port_bundle("axil_in", self.axil_if.input())
         self._clk = self.axil_in.ports.clk
         self._rst_n = self.axil_in.ports.resetn
 
-        self.axil_m = self.port_bundle("axil_m", self.axil_if.master())
         self.axil_s = self.port_bundle("axil_s", self.axil_if.slave())
 
         self.inst_config_arbiter()
@@ -27,9 +26,10 @@ class GlobalController(Generator):
         This function instantiates configuration arbiter
         '''
         self.add_child("cfg_arbiter",
-                GlcCfgArbiter(self.axil_awidth, self.axil_dwidth),
+                GlcCfgArbiter(self.p_axil_awidth, self.p_axil_dwidth),
                 comment="configuration arbiter")
-        self.wire(self.axil_m, self["cfg_arbiter"].axil_m)
+        self.wire(self._clk, self["cfg_arbiter"]._clk)
+        self.wire(self._rst_n, self["cfg_arbiter"]._rst_n)
         self.wire(self.axil_s, self["cfg_arbiter"].axil_s)
 
 if __name__ == "__main__":
