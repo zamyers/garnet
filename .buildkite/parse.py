ca873f99ac13b69e78f0d0365d68de9d56ddd31d
norabarlow <kathleen.feng0@gmail.com>
2019-10-22 18:27:28 -0700

    add parsing for pe

--- .buildkite/parse.py
+++ .buildkite/parse.py
@@ -16,7 +16,7 @@ def parse_memcore(filename):
         split = line.split()
         if len(split) > 4:
             field = split[0].strip()
-            total = float(split[-1].strip())
+            total = float(split[-1].strip())/10e9
 
             # UNIFIED/DOUBLE BUFFER CONTROLLER
             part_of_db = False
@@ -101,7 +101,45 @@ def parse_memcore(filename):
     print("OTHER2,%f" % (results["fifo_control"] + results["linebuffer_control"] + results["sram_control"] + other))
 
 def parse_pe(filename):
-    print("currently not implemented")
+    f = open(filename, 'r')
+
+    # Read past header
+    # Area report is 20, power report is 21
+    header = 20
+    for i in range(0, header):
+        f.readline()
+
+    results = {}
+
+    line = f.readline()
+    while line:
+        split = line.split()
+        if len(split) > 4:
+            field = split[0].strip()
+            total = float(split[-1].strip())/10e9
+
+            if field == "Tile_PE":
+                results[field] = total
+            if field == "PE_comb_inst0":
+                results["alu"] = total;
+            if field == "SB_ID0_5TRACKS_B16_PE" or field == "SB_ID0_5TRACKS_B1_PE":
+                if "SB" in results.keys():
+                    total += results["SB"]
+                results["SB"] = total
+            if line.startswith("  CB_"):
+                if "CB" in results.keys():
+                    total += results["CB"]
+                results["CB"] = total
+
+        line = f.readline()
+
+    other = results["Tile_PE"] - (results["alu"] + results["SB"] + results["CB"])
+    print("TOTAL,%f" % results["Tile_PE"])
+    print("ALU,%f" % results["alu"])
+    print("CONNECTION BOX,%f" % results["CB"])
+    print("SWITCH BOX,%f" % results["SB"])
+    print("OTHER,%f" % other)
+    #print(f"{results['alu']},{results['CB']},{results['SB']},{other}")
 
 def main():
     parser = argparse.ArgumentParser()
