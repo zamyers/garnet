9aff6695e8fa7f0f16bf7111ea5f5b504c506c5e
Alex Carsello <alex.carsello@gmail.com>
2020-03-09 21:29:37 -0700

    raise pe tile clk freq to 5 ns

--- mflowgen/Tile_PE/construct.py
+++ mflowgen/Tile_PE/construct.py
@@ -26,7 +26,7 @@ def construct():
   parameters = {
     'construct_path'    : __file__,
     'design_name'       : 'Tile_PE',
-    'clock_period'      : 4.0,
+    'clock_period'      : 5.0,
     'adk'               : adk_name,
     'adk_view'          : adk_view,
     # Synthesis
