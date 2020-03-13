43ea0559b3ad9db829073409a76bebc612ea0dda
Alex Carsello <alex.carsello@gmail.com>
2020-03-12 21:12:18 -0700

    Add VDD_SW to global_net_connect

--- mflowgen/common/power-scripts/globalnetconnect.tcl
+++ mflowgen/common/power-scripts/globalnetconnect.tcl
@@ -10,10 +10,24 @@
 
 # Connect VNW / VPW if any cells have these pins
 
-globalNetConnect VDD -type pgpin -pin VDD -inst *
-globalNetConnect VDD -type tiehi
-globalNetConnect VSS -type pgpin -pin VSS -inst *
-globalNetConnect VSS -type tielo
-globalNetConnect VDD -type pgpin -pin VPP -inst *
-globalNetConnect VSS -type pgpin -pin VBB -inst *
+# TODO: Make this not error out if PWR_AWARE doesn't exist
+if $::env(PWR_AWARE) {
+   # TODO: Make sure file named correctly
+   read_power_intent -1801 upf.tcl
+   commit_power_intent
+   write_power_intent -1801 upf.out
+
+   globalNetConnect VDD_SW -type tiehi -powerdomain TOP
+   globalNetConnect VDD    -type tiehi -powerdomain AON
+   globalNetConnect VSS -type tielo
+   globalNetConnect VDD -type pgpin -pin VPP -inst *
+   globalNetConnect VSS -type pgpin -pin VBB -inst *
+} else {
+   globalNetConnect VDD -type pgpin -pin VDD -inst *
+   globalNetConnect VDD -type tiehi
+   globalNetConnect VSS -type pgpin -pin VSS -inst *
+   globalNetConnect VSS -type tielo
+   globalNetConnect VDD -type pgpin -pin VPP -inst *
+   globalNetConnect VSS -type pgpin -pin VBB -inst *
+}
 
