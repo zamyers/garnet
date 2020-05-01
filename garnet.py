0802cbe9ee14f1081a195c8e488b0cbc2164433f
Ross <rdaly525@stanford.edu>
2020-04-30 21:58:55 -0700

    Added coreir pass to remove unused modules

--- garnet.py
+++ garnet.py
@@ -306,7 +306,7 @@ def main():
         garnet_circ = garnet.circuit()
         magma.compile("garnet", garnet_circ, output="coreir-verilog",
                       coreir_libs={"float_DW"},
-                      passes = ["rungenerators", "inline_single_instances", "clock_gate"])
+                      passes = ["rungenerators", "inline_single_instances", "clock_gate", "cullgraph"])
         garnet.create_stub()
     if len(args.app) > 0 and len(args.input) > 0 and len(args.gold) > 0 \
             and len(args.output) > 0:
